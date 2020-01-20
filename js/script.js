7c736d579a0a9086d61d8a41980f16189c8e94d8
Katy Solovewicz <katy@solovewicz.com>
2020-01-20 10:06:13 -0800

    add functions to each set of questions

--- js/script.js
+++ js/script.js
@@ -13,4 +13,36 @@ $(document).ready(function() {
 		event.preventDefault();
 	});
 
+		$(".question2").click(function(event){
+		$("#row2").hide();
+		$("#row3").removeClass("hidden");
+		var answer2 = $(this).val();
+		console.log ("answer 2: " + answer2);
+		event.preventDefault();
+	});
+
+		$(".question3").click(function(event){
+		$("#row3").hide();
+		$("#row4").removeClass("hidden");
+		var answer3 = $(this).val();
+		console.log ("answer 3: " + answer3);
+		event.preventDefault();
+	});
+
+		$(".question4").click(function(event){
+		$("#row4").hide();
+		$("#row5").removeClass("hidden");
+		var answer4 = $(this).val();
+		console.log ("answer 4: " + answer4);
+		event.preventDefault();
+	});
+
+		$(".question5").click(function(event){
+		$("#row5").hide();
+		$("#row6").removeClass("hidden");
+		var answer5 = $(this).val();
+		console.log ("answer 5: " + answer5);
+		event.preventDefault();
+	});
+
 });
