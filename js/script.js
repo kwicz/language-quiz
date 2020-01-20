eafd7aa782764a3be84c9867cc0a8dc3481ad4b0
Katy Solovewicz <katy@solovewicz.com>
2020-01-20 10:00:30 -0800

    add attributes to all potential survey answers

--- js/script.js
+++ js/script.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
 		$("#row1").removeClass("hidden");
 	});
 
-	$(".question1").click(function(){
+	$(".question1").click(function(event){
 		$("#row1").hide();
 		$("#row2").removeClass("hidden");
 		var answer1 = $(this).val();
 		console.log ("answer 1: " + answer1);
-		//event.preventDefault();
+		event.preventDefault();
 	});
 
 });
