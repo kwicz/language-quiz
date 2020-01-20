51a1b7e38adad1fa6905df29b2e32a3c154d5f27
Katy Solovewicz <katy@solovewicz.com>
2020-01-20 11:19:06 -0800

    add answer calculation function

--- js/script.js
+++ js/script.js
@@ -1,47 +1,73 @@
+
+//Declare global variables (sorry)
+var python = 0;
+var ruby = 0;
+var swift = 0;
+
+// Tally up user responses
+function calculate(response) {
+	if (response === "python") {
+		python++;
+	}
+	else if (response === "ruby") {
+		ruby++;
+	}
+	else if (response === "swift") {
+		swift++;
+	}
+};
+
+// Load the DOM
 $(document).ready(function() {
 
+	// Start the quiz
 	$("#start").click(function(){
 		$("#start").hide();
 		$("#row1").removeClass("hidden");
 	});
 
+	// Get answer to first question
 	$(".question1").click(function(event){
 		$("#row1").hide();
 		$("#row2").removeClass("hidden");
-		var answer1 = $(this).val();
-		console.log ("answer 1: " + answer1);
+		answer1 = $(this).val();
+		calculate(answer1);
 		event.preventDefault();
 	});
 
-		$(".question2").click(function(event){
+	// Get answer to second question
+	$(".question2").click(function(event){
 		$("#row2").hide();
 		$("#row3").removeClass("hidden");
 		var answer2 = $(this).val();
-		console.log ("answer 2: " + answer2);
+		calculate(answer2);
 		event.preventDefault();
 	});
 
-		$(".question3").click(function(event){
+	// Get answer to third question
+	$(".question3").click(function(event){
 		$("#row3").hide();
 		$("#row4").removeClass("hidden");
 		var answer3 = $(this).val();
-		console.log ("answer 3: " + answer3);
+		calculate(answer3);
 		event.preventDefault();
 	});
 
-		$(".question4").click(function(event){
+	// Get answer to fourth question
+	$(".question4").click(function(event){
 		$("#row4").hide();
 		$("#row5").removeClass("hidden");
 		var answer4 = $(this).val();
-		console.log ("answer 4: " + answer4);
+		calculate(answer4);
 		event.preventDefault();
 	});
 
-		$(".question5").click(function(event){
+	// Get answer to fifth question
+	$(".question5").click(function(event){
 		$("#row5").hide();
 		$("#row6").removeClass("hidden");
 		var answer5 = $(this).val();
-		console.log ("answer 5: " + answer5);
+		calculate(answer5);
 		event.preventDefault();
 	});
 
