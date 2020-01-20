0e77069a76bc302958aced8746ccff2bffddaf09
Katy Solovewicz <katy@solovewicz.com>
2020-01-20 13:03:05 -0800

    fix result logic

--- js/script.js
+++ js/script.js
@@ -32,6 +32,9 @@ $(document).ready(function() {
 		$("#row2").removeClass("hidden");
 		answer1 = $(this).val();
 		calculate(answer1);
+		console.log("python: " + python);
+		console.log("ruby: " + ruby);
+		console.log("swift: " + swift);
 		event.preventDefault();
 	});
 
@@ -41,6 +44,9 @@ $(document).ready(function() {
 		$("#row3").removeClass("hidden");
 		var answer2 = $(this).val();
 		calculate(answer2);
+				console.log("python: " + python);
+		console.log("ruby: " + ruby);
+		console.log("swift: " + swift);
 		event.preventDefault();
 	});
 
@@ -50,6 +56,9 @@ $(document).ready(function() {
 		$("#row4").removeClass("hidden");
 		var answer3 = $(this).val();
 		calculate(answer3);
+				console.log("python: " + python);
+		console.log("ruby: " + ruby);
+		console.log("swift: " + swift);
 		event.preventDefault();
 	});
 
@@ -59,6 +68,9 @@ $(document).ready(function() {
 		$("#row5").removeClass("hidden");
 		var answer4 = $(this).val();
 		calculate(answer4);
+				console.log("python: " + python);
+		console.log("ruby: " + ruby);
+		console.log("swift: " + swift);
 		event.preventDefault();
 	});
 
@@ -67,18 +79,21 @@ $(document).ready(function() {
 		$("#row5").hide();
 		var answer5 = $(this).val();
 		calculate(answer5);
+				console.log("python: " + python);
+		console.log("ruby: " + ruby);
+		console.log("swift: " + swift);
 		// Determine result
-		if (python > ruby && python > swift || python == ruby) {
+		if (python > ruby && python > swift || python == ruby && python > swift) {
 			$("ul").append('<li>Python</li>');
 			$("ul").append('<li><a href="https://www.learnpython.org/">Get Started Today!</a></li>');
 			$("ul").append('<li><img src="../css/python.jpeg"></li>');
 		}
-		else if (ruby > python && ruby > swift || ruby == swift) {
+		else if (ruby > python && ruby > swift || ruby == swift && ruby > python) {
 			$("ul").append('<li>Ruby</li>');
 			$("ul").append('<li><a href="https://www.ruby-lang.org/en/documentation/quickstart/">Get Started Today!</a></li>');
 			$("ul").append('<li><img src="../css/ruby.jpeg"></li>');
 		}
-		else if (swift > python && swift > ruby || swift == python) {
+		else if (swift > python && swift > ruby || swift == python && swift > ruby) {
 			$("ul").append('<li>Swift</li>');
 			$("ul").append('<li><a href="https://swift.org/getting-started/#installing-swift">Get Started Today!</a></li>');
 			$("ul").append('<li><img src="../css/swift.jpeg"></li>');
